65920e5037a43a5b622cd30071bd6404ae31950f
Вероника Мазуренко <veronikavv1732@gmail.com>
2025-11-07 12:18:51 +0700

    add tests

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -11,25 +11,20 @@ def test_log_file():
     my_function(2, 3)
     with open("../mylog.txt", encoding="utf-8") as f:
         line = f.readline()
-        assert line == f"Функция: my_function\n"
+        assert line == "Функция: my_function\n"
+
+
+@log(filename="../mylog.txt")
+def add_numbers(a, b):
+    return a + b
 
 
 @log()
-def test_my_function(capsys):
+def test_my_function_consol(capsys):
     result = my_function(2, 3)
     captured = capsys.readouterr()
     assert result == 5
     assert "" in captured.out
-
-
-def test_log(capsys):
-    @log()
-    def my_function(x, y):
-        return x + y
-
-    my_function(2, 3)
-    result = capsys.readouterr()
-    assert result.out == "Функция: my_function\n"
 
 
 if __name__ == "__main__":
