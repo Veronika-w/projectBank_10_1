daaedcdb36cd20afc5da506192bc58e2732f4269
Вероника Мазуренко <veronikavv1732@gmail.com>
2025-11-15 11:57:48 +0700

    add  funcs and tests

--- src/utils.py
+++ src/utils.py
@@ -5,14 +5,15 @@ def read_json_operation(path: str) -> list[dict]:
     """Функция, возвращает список словарей с данными о финансовых транзакциях"""
     try:
         with open(path, encoding="utf-8") as f:
-            try:
-                data = json.load(f)
-            except json.JSONDecodeError:
-                return []
+            data = json.load(f)
+            return data
     except FileNotFoundError:
+        print ("Файл не найден")
         return []
-    return data
+    except json.JSONDecodeError:
+        print("Ошибка декодирования файла")
+        return []
 
 
 if __name__ == "__main__":
-    print(read_json_operation("../data/operations.json"))
+    print(read_json_operation("C:/Users/i3/my_pj/pythonBank/data/operations.json"))
--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -1,28 +1,29 @@
+import os
 from unittest.mock import patch
-
-import pytest
-import requests
 
 from src.external_api import convert_to_rub
 
+API_KEY = os.getenv("API_KEY")
 
-@patch("requests.get")
+
+@patch("requests.request")
 def test_convert_to_rub(mock_get):
     transaction = {
-        "id": 596171168,
+        "id": 782295999,
         "state": "EXECUTED",
-        "date": "2018-07-11T02:26:18.671407",
-        "operationAmount": {"amount": "79931.03", "currency": {"name": "руб.", "code": "RUB"}},
-        "description": "Открытие вклада",
-        "to": "Счет 72082042523231456215",
+        "date": "2019-09-11T17:30:34.445824",
+        "operationAmount": {"amount": "54280.01", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод организации",
+        "from": "Счет 24763316288121894080",
+        "to": "Счет 96291777776753236930",
     }
-
+    mock_get.return_value.status_code = 200
     amount = float(transaction.get("operationAmount", {}).get("amount", 0))
     currency = transaction.get("operationAmount", {}).get("currency", {}).get("code", {})
     url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency}&amount={amount}"
-    mock_get.return_value.json.return_value = 79931.03
-    assert convert_to_rub(mock_get) == 79931.03
-    mock_get.assert_called_once_with(url)
+    mock_get.return_value.json.return_value = {"result": 4369103.090999}
+    assert convert_to_rub(transaction) == 4369103.090999
+    mock_get.assert_called_once_with("GET", url, headers={"apikey": API_KEY})
 
 
 @patch("requests.get")
@@ -33,4 +34,4 @@ def test_rub_convert_transaction(mock_get):
     transaction = {"operationAmount": {"amount": "1", "currency": {"code": "USD"}}}
 
     result = convert_to_rub(transaction)
-    assert result == 75.0
+    assert result == 80.491936
