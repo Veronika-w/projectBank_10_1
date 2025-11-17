3952fa083f3aa356afa59e97ef2307e512d963e7
Вероника Мазуренко <veronikavv1732@gmail.com>
2025-11-17 20:10:57 +0700

    add logging masks

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,14 @@
 import json
 from typing import Any
+import logging
+
+
+logger = logging.getLogger('utils')
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler('C:/Users/i3/my_pj/pythonBank//logs/utils.log', encoding='utf-8')
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def read_json_operation(path: str) -> Any:
