7a4897a8f96ffe47a139b3b9937edd3ce024e4a0
Вероника Мазуренко <veronikavv1732@gmail.com>
2025-11-17 20:50:00 +0700

    add logging utils

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,12 @@
 import json
 from typing import Any
 import logging
+import os
 
 
 logger = logging.getLogger('utils')
 logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler('C:/Users/i3/my_pj/pythonBank//logs/utils.log', encoding='utf-8')
+file_handler = logging.FileHandler('C:/Users/i3/my_pj/pythonBank/logs/utils.log', encoding='utf-8')
 file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
@@ -14,17 +15,29 @@ logger.addHandler(file_handler)
 def read_json_operation(path: str) -> Any:
     """Функция, возвращает список словарей с данными о финансовых транзакциях"""
     try:
-        with open(path, encoding="utf-8") as f:
-            data = json.load(f)
-            if not isinstance(data, list):
+        if not os.path.exists(path):
+            logger.error(f"Файл {path} не найден")
+            return []
+
+        logger.debug(f"Попытка загрузить файл: {path}")
+
+        with open(path, "r", encoding="utf-8") as file:
+            try:
+                data = json.load(file)
+                logger.debug("Данные успешно загружены из файла")
+
+                if not isinstance(data, list):
+                    logger.error("Данные в файле не являются списком")
+                    return []
+                logger.info("Данные успешно проверены и корректны")
+                return data
+
+            except json.JSONDecodeError:
+                logger.error("Ошибка декодирования JSON")
                 return []
-            return data
-    except FileNotFoundError:
-        print("Файл не найден")
-        return []
-    except json.JSONDecodeError:
-        print("Ошибка декодирования файла")
-        return []
+    except Exception:
+        logger.exception("Критическая ошибка при работе с файлом")
+    return []
 
 
 if __name__ == "__main__":
