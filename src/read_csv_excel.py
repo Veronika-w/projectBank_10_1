adf2296e741c3111491be402b769d4268410ed0b
Вероника Мазуренко <veronikavv1732@gmail.com>
2025-11-20 16:20:45 +0700

    add readme and funcs

--- src/read_csv_excel.py
+++ src/read_csv_excel.py
@@ -1,4 +1,4 @@
-import csv
+import json
 from typing import Any, Dict
 
 import pandas as pd
@@ -6,33 +6,25 @@ import pandas as pd
 
 def read_transactions_csv(file_path: str) -> list:
     """Считывает финансовые операции из CSV-файла и возвращает список словарей с транзакциями"""
-    try:
-        transactions_csv = []
-        with open(file_path, "r", newline="", encoding="utf-8") as csv_file:
-            rd_transactions_csv = csv.DictReader(csv_file)
-            next(rd_transactions_csv)
-            for row in rd_transactions_csv:
-                transactions_csv.append(row)
-        return transactions_csv
-    except FileNotFoundError:
-        return []
+    df = pd.read_csv(file_path)
+    df_dict = df.to_dict('records')
+    df_json = json.dumps(df_dict, ensure_ascii=False, indent=4)
+    return df_json
 
 
-if __name__ == "__main__":
-    print(read_transactions_csv("C:/Users/i3/my_pj/pythonBank/data/transactions.csv"))
+print(read_transactions_csv('../data/transactions.csv'))
 
 
 def read_transactions_excel(file_path: str) -> list[Dict[str, Any]]:
     """Функция считывает финансовые операции из Excel - файла и выдает список
     словарей с транзакциями."""
     try:
-        df = pd.read_excel(file_path)
-        df["id", "amount"] = df["id", "amount"].astype(int)
-        print(df.to_dict(orient="records"))
-
+        df_excel = pd.read_excel(file_path)
+        df_excel['id', 'amount'] = df_excel['id', 'amount'].astype(int)
+        print(df_excel.to_dict(orient='records'))
     except FileNotFoundError:
         return []
 
 
 if __name__ == "__main__":
-    print(read_transactions_excel("C:/Users/i3/my_pj/pythonBank/data/transactions_excel.xlsx"))
+    print(read_transactions_excel("C:/Users/PB/Desktop/Python/homework_10_1/data/transactions_excel.xlsx"))
