fa91dec5bbfb2e0576e2177d7526dafe5e8fdb42
Вероника Мазуренко <veronikavv1732@gmail.com>
2025-11-20 19:28:14 +0700

    add  funcs

--- src/read_csv_excel.py
+++ src/read_csv_excel.py
@@ -7,14 +7,14 @@ def read_transactions_csv(file_path: str) -> list:
     """Считывает финансовые операции из CSV-файла и возвращает список словарей с транзакциями"""
     try:
         df = pd.read_csv(file_path)
-        df_dict = df.to_dict('records')
+        df_dict = df.to_dict("records")
         df_json = json.dumps(df_dict, ensure_ascii=False, indent=4)
         return df_json
     except FileNotFoundError:
         return []
 
 
-print(read_transactions_csv('C:/Users/i3/my_pj/pythonBank/data/transactions.csv'))
+print(read_transactions_csv("C:/Users/i3/my_pj/pythonBank/data/transactions.csv"))
 
 
 def read_transactions_excel(path: str) -> list[dict]:
@@ -22,11 +22,11 @@ def read_transactions_excel(path: str) -> list[dict]:
     словарей с транзакциями."""
     try:
         df_excel = pd.read_excel(path)
-        transactions = df_excel.to_dict(orient='records')
+        transactions = df_excel.to_dict(orient="records")
         return transactions
     except Exception:
         return []
 
 
 if __name__ == "__main__":
-    print(read_transactions_excel('C:/Users/i3/my_pj/pythonBank/data/transactions_excel.xlsx'))
+    print(read_transactions_excel("C:/Users/i3/my_pj/pythonBank/data/transactions_excel.xlsx"))
