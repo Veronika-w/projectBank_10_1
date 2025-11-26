519fcda75cf987056e0939b39e7a76aa582a631c
VeronikaW <veronikavv1732@gmail.com>
2025-11-26 11:39:32 +0300

    creating an absolute path

--- src/masks.py
+++ src/masks.py
@@ -2,7 +2,7 @@ import logging
 
 logger = logging.getLogger("masks")
 logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler("C:/Users/i3/my_pj/pythonBank//logs/masks.log", encoding="utf-8")
+file_handler = logging.FileHandler("../logs/masks.log", encoding="utf-8")
 file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
