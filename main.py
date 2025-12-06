1fd6682a20ce0b412229a9ba8730fdc58562d116
Вероника Мазуренко <veronikavv1732@gmail.com>
2025-12-06 11:14:54 +0300

    add funcs main bank_operations

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-from src.masks import *
+from src.bank_operations import process_bank_search
+from src.processing import sort_by_date
+from src.read_csv_excel import read_transactions_csv, read_transactions_excel
+from src.utils import read_json_operation
 
-print(get_mask_card_number("7000792289606361"))
-print(get_mask_account("73654108430135874305"))
+
+def main ():
+    print("Привет! Добро пожаловать в программу работы с банковскими транзакциями.\n"
+          "Выберите необходимый пункт меню:\n"
+          "1. Получить информацию о транзакциях из JSON-файла\n"
+          "2. Получить информацию о транзакциях из CSV-файла\n"
+          "3. Получить информацию о транзакциях из XLSX-файла")
---
+++ src/bank_operations.py
@@ -0,0 +1,14 @@
+import re
+
+from src.read_csv_excel import read_transactions_excel
+
+
+def process_bank_search(data:list[dict], search:str)->list[dict]:
+    pattern = re.compile(re.escape[search]), re.IGNORECASE
+
+
+
+def process_bank_operations(data:list[dict], categories:list)->dict:
+
+
+
