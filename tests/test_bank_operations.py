1a881a313c3ad4dfc5e4172a54a7f91d21a090ba
Вероника Мазуренко <veronikavv1732@gmail.com>
2025-12-28 23:24:01 +0300

    add tests

--- tests/test_bank_operations.py
+++ tests/test_bank_operations.py
@@ -3,7 +3,7 @@ from src.bank_operations import process_bank_operations, process_bank_search
 
 def test_process_bank_search_match() -> None:
     transactions = [
-        {"description": "Покупка продуктов"},
+        {"description": "Перевод с карты на карту"},
         {"description": "Оплата услуг"},
         {"description": "Перевод организации"},
         {"description": "Открытие вклада"},
@@ -11,11 +11,11 @@ def test_process_bank_search_match() -> None:
     search_string = "Перевод организации"
     assert process_bank_search(transactions, search_string) == [{"description": "Перевод организации"}]
 
+    search_string_2 = "Перевод с карты на карту"
+    assert process_bank_search(transactions, search_string_2) == [{"description": "Перевод с карты на карту"}]
+
+
 def test_process_bank_operations() -> None:
     assert process_bank_operations([], []) == {}
     assert process_bank_operations([], ["Перевод организации", "Открытие вклада",
                                         "Перевод с карты на счет", "Перевод с карты на карту"]) == {}
-
-
-
-
